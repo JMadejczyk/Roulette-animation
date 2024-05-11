2ed77e25d6bb0b17dc46d5da783daf798f0c7f40
Jakub Madejczyk <jbmk01@gmail.com>
2024-05-11 10:04:41 +0200

    colors changed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 import { motion } from "framer-motion";
 import { useEffect, useState } from "react";
 
-export default function Home() {
+export default function Roulette() {
   const teams: string[] = [
     "ZSK",
     "ZSŁ",
@@ -14,15 +14,25 @@ export default function Home() {
     "11lo",
   ];
   const teams_colors: any = {
-    ZSK: "bg-red-500",
-    ZSŁ: "bg-blue-500",
-    Marynka: "bg-green-500",
-    "17lo": "bg-yellow-500",
-    "15lo": "bg-purple-500",
-    "3lo": "bg-pink-500",
-    "8lo": "bg-indigo-500",
-    "11lo": "bg-gray-500",
+    // ZSK: "bg-red-500",
+    // ZSŁ: "bg-blue-500",
+    // Marynka: "bg-green-500",
+    // "17lo": "bg-yellow-500",
+    // "15lo": "bg-purple-500",
+    // "3lo": "bg-pink-500",
+    // "8lo": "bg-indigo-500",
+    // "11lo": "bg-gray-500",
+    ZSK: "bg-gradient-to-r from-[#f72585] to-[#b5179e]",
+    ZSŁ: "bg-gradient-to-r from-[#b5179e] to-[#7209b7]",
+    Marynka: "bg-gradient-to-r from-[#7209b7] to-[#560bad]",
+    "17lo": "bg-gradient-to-r from-[#560bad] to-[#480ca8]",
+    "15lo": "bg-gradient-to-r from-[#480ca8] to-[#3f37c9]",
+    "3lo": "bg-gradient-to-r from-[#3f37c9] to-[#4361ee]",
+    "8lo": "bg-gradient-to-r from-[#4361ee] to-[#4895ef]",
+    "11lo": "bg-gradient-to-r from-[#4895ef] to-[#4cc9f0]",
+    // TEB: "bg-gradient-to-r from-[#4361ee] to-[#4895ef]",
   };
+  const numberOfGroups = 4;
 
   const [teamsRandom, setTeamsRandom] = useState<string[][]>([[], [], [], []]);
 
@@ -36,6 +46,7 @@ export default function Home() {
     }
     return true;
   };
+
   useEffect(() => {
     let teamsRandomCopy = teamsRandom.concat();
 
@@ -48,14 +59,42 @@ export default function Home() {
           teamsRandomCopy[i] = [...teamsRandomCopy[i], teams[j % teams.length]];
         }
         teamsRandomCopy[i].sort(() => Math.random() - 0.5);
-
         k += 5;
       }
     }
     setTeamsRandom(teamsRandomCopy);
   }, []);
 
-  console.log(teamsRandom);
+  // console.log(teamsRandom);
+
+  let teamsRenderArray = [];
+  for (let i = 0; i < numberOfGroups; i++) {
+    teamsRenderArray.push(
+      <motion.div
+        key={i + "column"}
+        className={`w-36 bg-black flex flex-col items-center`}
+        initial={{ y: -100 * teamsRandom[i].length }}
+        animate={{ y: -40 }}
+        transition={{
+          duration: 4,
+          type: "spring",
+          delay: (i + 1) * 0.5,
+          bounce: 0.2,
+          ease: "easeInOut",
+        }}
+        //TODO fix the animation for every screen size
+      >
+        {teamsRandom[i].map((team, index) => (
+          <div
+            key={index}
+            className={`w-full text-white pt-14 pb-14 border text-center ${teams_colors[team]}`}
+          >
+            {team}
+          </div>
+        ))}
+      </motion.div>
+    );
+  }
 
   if (teamsRandom[0].length === 0) {
     return (
@@ -65,99 +104,19 @@ export default function Home() {
     );
   } else if (teams && teams_colors) {
     return (
-      <div className="bg-gradient-to-b from-blue-500 to-purple-500 w-screen h-screen flex justify-center items-center">
-        <div className="w-[60vw] h-[60vh] bg-slate-400 rounded-2xl flex justify-center gap-10 overflow-hidden relative pr-10 pl-10">
-          <div className="bg-gradient-to-b from-slate-600 to-transparent z-20 absolute top-0 h-[20vh] w-full"></div>
-          <div className="bg-gradient-to-b from-slate-600 to-transparent z-20 absolute top-0 h-[20vh] w-full"></div>
-          <div className="bg-gradient-to-b from-slate-600 to-transparent z-20 absolute top-0 h-[20vh] w-full"></div>
-          <div className="bg-gradient-to-t from-slate-600 to-transparent z-20 absolute bottom-0 h-[20vh] w-full"></div>
-          <div className="bg-gradient-to-t from-slate-600 to-transparent z-20 absolute bottom-0 h-[20vh] w-full"></div>
-          <div className="bg-gradient-to-t from-slate-600 to-transparent z-20 absolute bottom-0 h-[20vh] w-full"></div>
+      <div className="bg-gradient-to-b from-blue-500 to-purple-500 w-screen h-screen flex justify-center items-center ">
+        <div className="w-[60vw] h-[60vh] bg-slate-400 rounded-2xl flex justify-center gap-10 overflow-hidden relative pr-10 pl-10 shadow-2xl">
+          <div className="bg-gradient-to-b from-slate-600 to-transparent z-20 absolute top-0 h-[22vh] w-full"></div>
+          <div className="bg-gradient-to-b from-slate-600 to-transparent z-20 absolute top-0 h-[22vh] w-full"></div>
+          <div className="bg-gradient-to-b from-slate-600 to-transparent z-20 absolute top-0 h-[22vh] w-full"></div>
+          <div className="bg-gradient-to-b from-slate-600 to-transparent z-20 absolute top-0 h-[22vh] w-full"></div>
 
-          <motion.div
-            className={`w-36 bg-black flex flex-col items-center`}
-            initial={{ y: -100 * teamsRandom[0].length }}
-            animate={{ y: -61 }}
-            transition={{
-              duration: 4,
-              type: "spring",
-              delay: 0.5,
-              bounce: 0.2,
-              ease: "easeInOut",
-            }}
-          >
-            {teamsRandom[0].map((team, index) => (
-              <div
-                key={index}
-                className={`w-full text-white pt-14 pb-14 border text-center ${teams_colors[team]}`}
-              >
-                {team}
-              </div>
-            ))}
-          </motion.div>
-          <motion.div
-            className="w-36 bg-black flex flex-col items-center"
-            initial={{ y: -100 * teamsRandom[1].length }}
-            animate={{ y: -61 }}
-            transition={{
-              duration: 4,
-              type: "spring",
-              delay: 1,
-              bounce: 0.2,
-              ease: "easeInOut",
-            }}
-          >
-            {teamsRandom[1].map((team, index) => (
-              <div
-                key={index}
-                className={`w-full text-white pt-14 pb-14 border text-center ${teams_colors[team]}`}
-              >
-                {team}
-              </div>
-            ))}
-          </motion.div>
-          <motion.div
-            className="w-36 bg-black flex flex-col items-center"
-            initial={{ y: -100 * teamsRandom[2].length }}
-            animate={{ y: -61 }}
-            transition={{
-              duration: 4,
-              type: "spring",
-              delay: 1.5,
-              bounce: 0.2,
-              ease: "easeInOut",
-            }}
-          >
-            {teamsRandom[2].map((team, index) => (
-              <div
-                key={index}
-                className={`w-full text-white pt-14 pb-14 border text-center ${teams_colors[team]}`}
-              >
-                {team}
-              </div>
-            ))}
-          </motion.div>
-          <motion.div
-            className="w-36 bg-black flex flex-col items-center"
-            initial={{ y: -100 * teamsRandom[3].length }}
-            animate={{ y: -61 }}
-            transition={{
-              duration: 4,
-              type: "spring",
-              delay: 2,
-              bounce: 0.2,
-              ease: "easeInOut",
-            }}
-          >
-            {teamsRandom[3].map((team, index) => (
-              <div
-                key={index}
-                className={`w-full text-white pt-14 pb-14 border text-center ${teams_colors[team]} `}
-              >
-                {team}
-              </div>
-            ))}
-          </motion.div>
+          <div className="bg-gradient-to-t from-slate-600 to-transparent z-20 absolute bottom-0 h-[22vh] w-full"></div>
+          <div className="bg-gradient-to-t from-slate-600 to-transparent z-20 absolute bottom-0 h-[22vh] w-full"></div>
+          <div className="bg-gradient-to-t from-slate-600 to-transparent z-20 absolute bottom-0 h-[22vh] w-full"></div>
+          <div className="bg-gradient-to-t from-slate-600 to-transparent z-20 absolute bottom-0 h-[22vh] w-full"></div>
+
+          {teamsRenderArray}
         </div>
       </div>
     );
